96153a838748402dd4eb850748a31408972ec7e0
Kieran Lynch <lynch.kieran1@gmail.com>
2020-06-16 17:32:29 -0500

    completed api call for first weather data div and inserted info into the dom

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// temp
-// humidity
-// wind Speed
-// UV index
-
-
 
 
 function renderWeatherInfo() {
@@ -15,18 +9,44 @@ function renderWeatherInfo() {
         method: 'GET',
     }).then(function(response) {
         var name = response.city.name;
+        var temp = "Temperature: " + (response.list[0].main.temp) + " °F";
+        var humidity = "Humidity: " + response.list[0].main.humidity + "%";
+        var windSpeed = "Wind Speed: " + response.list[0].wind.speed + " MPH";
         var cityHeading = $("<h3>");
         cityHeading.text(name);
         $("#cityName").append(cityHeading);
-        var temp = "Temperature: " + (response.list[0].main.temp) + " °F";
         var tempP = $("<p>");
         tempP.text(temp);
         $("#weatherData").append(tempP);
+        var humidP = $("<p>");
+        humidP.text(humidity);
+        $("#weatherData").append(humidP);
+        var windP = $("<p>");
+        windP.text(windSpeed);
+        $("#weatherData").append(windP);
+
+        // calling UV api
+        var lat = response.city.coord.lat;
+        var long = response.city.coord.lon;
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + long;
+
+        $.ajax({
+            url: uvURL,
+            method: 'GET',
+        }).then(function(result) {
+            var uvIndex = "UV Index: " + result.value;
+            var uvP = $("<p>");
+            uvP.text(uvIndex);
+            $("#weatherData").append(uvP);
+        })
+
     })
 }
 
 $("#citySearchBtn").on("click", function(event) {
     event.preventDefault();
+    $("#cityName").empty();
+    $("#weatherData").empty();
     var city = $("#searchInput").val();
     var newLi = $('<li class="previousCities">');
     newLi.attr("data-name", city);
