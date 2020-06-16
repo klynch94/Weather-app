a33c2e27fe8a34bdf546bd3fcae1e331948c941e
Kieran Lynch <lynch.kieran1@gmail.com>
2020-06-16 15:26:30 -0500

    created gitignore file and started to create api call

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 
-var queryURL = "";
 
+
+
+function renderWeatherInfo() {
+    var city = $("#searchInput").val();
+    var queryURL = "";
+    console.log(city);
+}
 
 $("#citySearchBtn").on("click", function(event) {
     event.preventDefault();
     var city = $("#searchInput").val();
     var newLi = $('<li class="previousCities">');
+    newLi.attr("data-name", city);
     newLi.text(city);
     $("#searched-cities").append(newLi);
+    renderWeatherInfo();
 })
 
