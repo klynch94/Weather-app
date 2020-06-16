3db38045cd04f10175b7bfa8ec54a22976b2b3e4
Kieran Lynch <lynch.kieran1@gmail.com>
2020-06-16 16:59:27 -0500

    created API call to begin calling weather data

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+// temp
+// humidity
+// wind Speed
+// UV index
 
 
 
 
 function renderWeatherInfo() {
     var city = $("#searchInput").val();
-    var queryURL = "";
-    console.log(city);
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function(response) {
+        var name = response.city.name;
+        var cityHeading = $("<h3>");
+        cityHeading.text(name);
+        $("#cityName").append(cityHeading);
+        var temp = "Temperature: " + (response.list[0].main.temp) + " °F";
+        var tempP = $("<p>");
+        tempP.text(temp);
+        $("#weatherData").append(tempP);
+    })
 }
 
 $("#citySearchBtn").on("click", function(event) {
