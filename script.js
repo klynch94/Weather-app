97ee101d675a899f3de659bba3c3fd3911ca7837
Kieran Lynch <lynch.kieran1@gmail.com>
2020-06-17 13:09:39 -0500

    added icons to 5 day forecast and city name

--- script.js
+++ script.js
@@ -29,8 +29,12 @@ function renderWeatherInfo() {
         var humidity = "Humidity: " + response.list[0].main.humidity + "%";
         var windSpeed = "Wind Speed: " + response.list[0].wind.speed + " MPH";
         var cityHeading = $("<h3>");
+        var currentIconImg = $("<img>");
+        var currentIconURL = "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png";
+        currentIconImg.attr("src", currentIconURL);
         cityHeading.text(name);
         $("#cityName").append(cityHeading);
+        $("#cityName").append(currentIconImg);
         var tempP = $("<p>");
         tempP.text(temp);
         $("#weatherData").append(tempP);
@@ -44,16 +48,24 @@ function renderWeatherInfo() {
         // getting data for next 5 days by looking for 12:00:00 on each list
         for(var i=0; i<response.list.length; i++) {
             if(response.list[i].dt_txt.includes("12:00:00")) {
-                console.log(response.list[i]);
-                var dayTemp = "Temperature: " + Math.floor((response.list[i].main.temp)) + " °F";
-                console.log(dayTemp);
+                var dayTemp = "Temp: " + Math.floor((response.list[i].main.temp)) + " °F";
+                var dayHumid = "Humidity: " + response.list[i].main.humidity + "%";
+                var iconImg = $("<img>");
+                var iconURL = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png";
+                console.log(iconURL);
+                iconImg.attr("src", iconURL);
                 var card = $("<div>");
                 card.addClass("card");
                 var cardBody = $("<div>");
                 cardBody.attr("class", "card-body");
-                var newP = $("<p>");
-                newP.text(dayTemp);
-                cardBody.append(newP);
+                cardBody.attr("class", "container");
+                var oneP = $("<p>");
+                var twoP = $("<p>");
+                oneP.text(dayTemp);
+                twoP.text(dayHumid);
+                cardBody.append(iconImg);
+                cardBody.append(oneP);
+                cardBody.append(twoP);
                 card.append(cardBody);
                 $("#cardHolder").append(card);
             }
